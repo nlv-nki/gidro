a3db20a5d0feed1c4b46c66c44547127d51877de
Nikolai <nlv.nki@gmail.com>
2020-12-18 15:26:42 +0300

    configured static port

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,7 +1,9 @@
-module.exports = function() {
-    $.gulp.task('serve', function() {
-        $.browserSync.init({
-            server: './public'
-        });
+module.exports = function () {
+  $.gulp.task('serve', function () {
+    $.browserSync.init({
+      server: './public',
+      port: 8080,
+
     });
+  });
 };
